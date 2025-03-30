9fb2fb9fff4f14399ea33e1829b51066deb1de8f
guptaishita2606 <guptaishu2603@gmail.com>
2025-03-31 01:00:14 +0530

    Force JSONParser for DRF UI compatibility

--- ai_backend/settings.py
+++ ai_backend/settings.py
@@ -77,7 +77,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
 }
+
 
 WSGI_APPLICATION = "ai_backend.wsgi.application"
 
